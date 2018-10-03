f27fe1f3d8a0113769f5e32cd9fb2d8616f6a566
Dr.clark <gura3_str89@icloud.com>
2018-10-04 00:32:49 +0900

    途中-_-

---
+++ Assets/EkispertWebService/Scripts/Editor/routeInfo.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Assets.EkispertWebService.Scripts.Editor
+{
+    class routeInfo
+    {
+        /// <summary>
+        /// 時間
+        /// </summary>
+        public string Time { get; set; }
+
+        /// <summary>
+        /// Transfer Count : 乗り換え回数
+        /// </summary>
+        public int TCount { get; set; }
+
+        /// <summary>
+        /// 片道金額
+        /// </summary>
+        public int Money { get; set; }
+
+        /// <summary>
+        /// 番線
+        /// </summary>
+        public int TLine { get; set; }
+
+        /// <summary>
+        /// 乗り換え時間
+        /// </summary>
+        public string[] TransTime = new string[2];
+    }
+}
